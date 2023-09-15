e308dca06c524fba50309b555b8b4f3c127703a2
FALEICA <fabio.leite.cardoso@gmail.com>
2023-09-14 23:24:43 -0300

    cadastro de usuario e tabela feita

--- src/main/java/erp_faleica/controllers/CadastroUsuarioBean.java
+++ src/main/java/erp_faleica/controllers/CadastroUsuarioBean.java
@@ -6,14 +6,46 @@ import java.util.List;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import erp_faleica.models.EstruturaSistemaModel;
-import erp_faleica.repositorio.EstruturaSistemaDAO;
+import erp_faleica.models.User;
+import erp_faleica.repositorio.UserDAO;
 
 @Named(value = "cadUserBean")
 @ViewScoped
 public class CadastroUsuarioBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	
+	
+	private User user = new User();
+	private UserDAO userDao;
+	
+	
+	
+	public void saveUser() {
+		userDao = new UserDAO();
+		userDao.saveUser(user);		
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public List<User> getAllUsers(){
+		userDao = new UserDAO();
+		 return userDao.getUserAll();		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
